0cb91d3a2d3a3228a6817c27004cdcb6305a1064
fengabner <fengabner@gmail.com>
2022-03-26 23:44:02 +0800

    springboot listener test

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/event/ApplicationStartedEvent.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/event/ApplicationStartedEvent.java
@@ -36,18 +36,20 @@ public class ApplicationStartedEvent extends SpringApplicationEvent {
 
 	/**
 	 * Create a new {@link ApplicationStartedEvent} instance.
+	 *
 	 * @param application the current application
-	 * @param args the arguments the application is running with
-	 * @param context the context that was being created
+	 * @param args        the arguments the application is running with
+	 * @param context     the context that was being created
 	 */
 	public ApplicationStartedEvent(SpringApplication application, String[] args,
-			ConfigurableApplicationContext context) {
+								   ConfigurableApplicationContext context) {
 		super(application, args);
 		this.context = context;
 	}
 
 	/**
 	 * Return the application context.
+	 *
 	 * @return the context
 	 */
 	public ConfigurableApplicationContext getApplicationContext() {
